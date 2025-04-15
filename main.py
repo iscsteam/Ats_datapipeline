094c6e2a017f6bf885e948de332463c5f10b652d
iscsteam <iscsdsteam@gmail.com>
2025-04-15 16:41:01 +0530

    Update main.py for cors error

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ origins = [
     "https://attendance-tracking.vercel.app",
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000"
 ]
 app.add_middleware(
     CORSMiddleware,
